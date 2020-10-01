1c91ec17b93fddf00c5c2bd33ba0a2e52e450152
kviatkovskii <sergejkvyatkovskij@gmail.com>
2020-10-01 11:28:45 +0300

    added a main protocol

---
+++ KVKToast/Classes/ToastModel.swift
@@ -0,0 +1,29 @@
+//
+//  ToastModel.swift
+//  KVKToast
+//
+//  Created by Sergei Kviatkovskii on 01.10.2020.
+//
+
+import Foundation
+
+public enum ToastPosition: Int {
+    case top, center, bottom
+}
+
+public enum ToastType: Int {
+    case info, success, warning, error
+    
+    func notificationFeedback() {
+        switch self {
+        case .info:
+            UIImpactFeedbackGenerator(style: .light).impactOccurred()
+        case .warning:
+            UINotificationFeedbackGenerator().notificationOccurred(.warning)
+        case .success:
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+        case .error:
+            UINotificationFeedbackGenerator().notificationOccurred(.error)
+        }
+    }
+}
---
+++ KVKToast/Classes/ToastProtocol.swift
@@ -0,0 +1,36 @@
+//
+//  ToastProtocol.swift
+//  KVKToast
+//
+//  Created by Sergei Kviatkovskii on 01.10.2020.
+//
+import UIKit
+
+public protocol KVKToastDisplayable {
+    func displayToast(_ title: String, message: String?, image: UIImage?, position: ToastPosition, type: ToastType, duration: Double, style: ToastStyle)
+}
+
+public extension KVKToastDisplayable {
+    func displayToast(_ title: String, message: String? = nil, image: UIImage? = nil, position: ToastPosition = .top, type: ToastType = .info, duration: Double = 3.0, style: ToastStyle = ToastStyle()) {
+        displayToast(title, message: message, image: image, position: position, type: type, duration: duration, style: style)
+    }
+}
+
+extension KVKToastDisplayable where Self: UIViewController {
+    public func displayToast(_ title: String, message: String?, image: UIImage?, position: ToastPosition, type: ToastType, duration: Double, style: ToastStyle) {
+        view.showToast(title: title, message: message, image: image, position: position, duration: duration, style: style)
+        type.notificationFeedback()
+    }
+}
+
+// MARK: Private Protocol
+
+protocol ToastProtocol: class {
+    func showToast(title: String, message: String?, image: UIImage?, position: ToastPosition, duration: Double, style: ToastStyle)
+}
+
+extension UIView: ToastProtocol {
+    func showToast(title: String, message: String?, image: UIImage?, position: ToastPosition, duration: Double, style: ToastStyle) {
+        
+    }
+}
