772c6e685da63422f6d58a4be7afd8cd2011c73f
kviatkovskii <sergejkvyatkovskij@gmail.com>
2020-11-10 12:51:43 +0300

    calculate width and height

--- KVKToast/Classes/ToastView.swift
+++ KVKToast/Classes/ToastView.swift
@@ -41,6 +41,7 @@ final class ToastView: UIView {
     private let messageLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
+        label.numberOfLines = 0
         label.adjustsFontSizeToFitWidth = true
         label.minimumScaleFactor = 0.8
         return label
@@ -50,7 +51,8 @@ final class ToastView: UIView {
         self.parameters = parameters
         super.init(frame: frame)
         
-        backgroundColor = .systemRed
+        backgroundColor = .systemTeal
+        //setBlur(style: UIScreen.isDarkMode ? .dark : .light)
         if let img = parameters.image {
             imageView.image = img
             imageView.frame = CGRect(x: 10, y: (frame.height * 0.5) - 12.5, width: 25, height: 25)
@@ -68,23 +70,27 @@ final class ToastView: UIView {
         }
         let titleHeight: CGFloat
         if parameters.message != nil {
-            titleHeight = (frame.height * 0.5) - 5
+            titleHeight = 25
         } else {
             titleHeight = frame.height
         }
         
         titleLabel.text = parameters.title
-        titleLabel.frame = CGRect(x: titleX, y: 5, width: titleWidth, height: titleHeight)
+        titleLabel.frame = CGRect(x: titleX, y: 0, width: titleWidth, height: titleHeight)
         addSubview(titleLabel)
         
         if let message = parameters.message {
             messageLabel.text = message
-            messageLabel.frame = CGRect(x: titleLabel.frame.origin.x, y: titleLabel.frame.height + 5, width: titleLabel.frame.width, height: titleLabel.frame.height)
+            messageLabel.frame = CGRect(x: titleLabel.frame.origin.x, y: titleLabel.frame.height, width: titleLabel.frame.width, height: frame.height - titleLabel.frame.height)
             addSubview(messageLabel)
         }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        //setBlur(style: UIScreen.isDarkMode ? .dark : .light)
     }
 }
