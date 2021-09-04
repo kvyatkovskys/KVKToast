18d171f5c5eb98c697954b4b9296df3dd8e352cf
kviatkovskii <sergejkvyatkovskij@gmail.com>
2021-09-04 21:35:51 +0500

    fixes for example

--- KVKToats-Example/ViewController.swift
+++ KVKToats-Example/ViewController.swift
@@ -9,27 +9,43 @@ import UIKit
 import KVKToast
 
 final class ViewController: UIViewController, KVKToastDisplayable {
+        
+    private lazy var displayButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Display", for: .normal)
+        button.isSelected = false
+        button.addTarget(self, action: #selector(controlToasts), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
-            self?.displayToast("Test Toast!",
-                               message: "Success!",
-                               position: .top,
-                               duration: 5)
-            self?.displayToast("Test Toast!",
-                               message: "Dispose of any resources!",
-                               position: .center,
-                               duration: 10)
-            self?.displayToast("Test Toast!",
-                               message: "Dispose of any resources!\nTest image display text",
-                               position: .bottom,
-                               duration: 20)
-        }
-        
+        displayButton.frame = CGRect(x: 0, y: 200, width: view.bounds.width, height: 50)
+        view.addSubview(displayButton)
     }
 
+    @objc private func controlToasts(sender: UIButton) {
+        sender.isSelected.toggle()
+        
+        if !sender.isSelected {
+            removeAllToasts()
+            sender.setTitle("Display", for: .normal)
+        } else {
+            displayToast("Test Toast!",
+                         message: "Success!",
+                         position: .top,
+                         duration: 5)
+            displayToast("Test Toast!",
+                         message: "Dispose of any resources!",
+                         position: .center,
+                         duration: 10)
+            displayToast("Test Toast!",
+                         message: "Dispose of any resources!\nTest image display text",
+                         position: .bottom,
+                         duration: 20)
+            sender.setTitle("Hide", for: .normal)
+        }
+    }
 
 }
 
